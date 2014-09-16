76e86c9d2ccc33c12dbc57b3207182f3df4bd916
beltex <cse93251@cse.yorku.ca>
2014-09-15 21:56:25 -0400

    Basic ncurses init

--- dash/main.swift
+++ dash/main.swift
@@ -1,3 +1,20 @@
 import Foundation
 
-println("Hello, World!")
+
+// ncurses settings
+initscr()               // Init window
+noecho()                // Don't echo user input
+nonl()                  // Disable newline mode
+keypad(stdscr, true)    // Enable function and arrow keys
+curs_set(0)             // Set cursor to invisible
+
+
+// Sample box
+var win = newwin(2, 30, 0, 0)
+box(win, CWideChar(" ").value, CWideChar(" ").value)
+wrefresh(win)
+
+getchar()
+
+
+endwin()    // Close window
