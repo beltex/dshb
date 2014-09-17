25e2f66bd30747ec4179d6e7583c55d850b84758
beltex <cse93251@cse.yorku.ca>
2014-09-16 22:05:37 -0400

    Minor: term colours check and set

--- dash/main.swift
+++ dash/main.swift
@@ -9,6 +9,15 @@ keypad(stdscr, true)    // Enable function and arrow keys
 curs_set(0)             // Set cursor to invisible
 
 
+if (!has_colors()) {
+    println("WARN: Terminal doesn't have support for colours")
+}
+
+
+// Init terminal colours
+start_color()
+
+
 // Sample box
 var win = newwin(2, 30, 0, 0)
 box(win, CWideChar(" ").value, CWideChar(" ").value)
