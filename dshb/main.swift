c8b219bce6cd622cb33a429eb904507cff63d8ef
beltex <beltex@users.noreply.github.com>
2014-12-11 21:15:46 -0500

    Cleanup to main.swift

--- dshb/main.swift
+++ dshb/main.swift
@@ -4,49 +4,23 @@ import Darwin
 import Dispatch
 import Foundation
 
+//------------------------------------------------------------------------------
+// MARK: GLOBAL STRUCTS
+//------------------------------------------------------------------------------
+
 
 /**
-App version.
-
-TODO: -v command line arg
+Like an ncurses window
 */
-let VERSION = "0.0.1"
-let MAX_WIDTH = 20.0
-
-var HAS_BATTERY = false
-var HAS_SMC     = false
-
-
-// TODO: This should probably be a serial queue
-// TODO: Custom serial attr prop creation causes runtime crash - Swift bug?
-var source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER,
-                                    0, 0,
-                                    dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0))
-
-dispatch_source_set_timer(source, dispatch_time(DISPATCH_TIME_NOW, 0), 1 * NSEC_PER_SEC, 0)
-
-//
-//dispatch_source_set_event_handler(source, {
-//    // getyx is complex macro
-//    var x = getcurx(stdscr)
-//    var y = getcury(stdscr)
-//    
-//    //getyx(stdscr,row,col)
-//    move(20, 20)
-//    addstr("HELLO - YEAH GCD")
-//    move(y, x)
-//    refresh()
-//})
-//
-//// We have to resume the the dispatch source as it is paused by default
-//dispatch_resume(source)
-
-
-
-public struct Window {
-    var size  : (length : Int32, width : Int32) = (0, 0)
-    var pos   : (x : Int32, y : Int32) = (0, 0)
+struct Window {
+    var size : (length: Int32, width: Int32) = (0, 0)
+    var pos  : (x: Int32, y: Int32) = (0, 0)
 }
+
+
+//------------------------------------------------------------------------------
+// MARK:  GLOBAL PROTOCOLS
+//------------------------------------------------------------------------------
 
 
 protocol Widget {
@@ -55,29 +29,79 @@ protocol Widget {
 }
 
 
+//------------------------------------------------------------------------------
+// MARK: GLOBAL PROPERTIES
+//------------------------------------------------------------------------------
+
+
+/**
+dshb version
+*/
+let DSHB_VERSION = "0.0.1"
+
+
+/**
+Does the machine have a battery?
+*/
+var HAS_BATTERY = false
+
+
+/**
+Does the machine have a SMC (System Management Controller)?
+*/
+var HAS_SMC = false
+
+
+/**
+Statistic update frequency in seconds
+*/
+var FREQ: UInt64 = 1
+
+
+/**
+Gap between widgets
+*/
+var gap: Int32 = 1
+
+
+/**
+Statistic widgets that are on (displayed)
+*/
+var widgets = [Widget]()
+
+
+// TODO: This should probably be a serial queue
+// TODO: Custom serial attr prop creation causes runtime crash - Swift bug?
+var source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,
+                                    dispatch_get_global_queue(
+                                               DISPATCH_QUEUE_PRIORITY_HIGH, 0))
+
+
+
+//------------------------------------------------------------------------------
+// MARK: NCURSES SETTINGS
+//------------------------------------------------------------------------------
+
 
 setlocale(LC_ALL, "")
-
-// ncurses settings
-initscr()                // Init window. Must be first
+initscr()                   // Init window. Must be first
 cbreak()
-
-//nodelay(stdscr, true)    // Make input reads non-blocking
-
-noecho()                 // Don't echo user input
-nonl()                   // Disable newline mode
-intrflush(stdscr, true) // Prevent flush
-keypad(stdscr, true)     // Enable function and arrow keys
+//nodelay(stdscr, true)     // Make input reads non-blocking - WARN - HIGH CPU!!
+noecho()                    // Don't echo user input
+nonl()                      // Disable newline mode
+intrflush(stdscr, true)     // Prevent flush
+keypad(stdscr, true)        // Enable function and arrow keys
 //timeout(0)
-curs_set(0)              // Set cursor to invisible
+curs_set(0)                 // Set cursor to invisible
 //idlok(stdscr, true)
 //scrollok(stdscr, true)
 
 
+// TODO: If no colour support, is it still safe to make colour related ncurses
+//       calls?
 if (!has_colors()) {
     println("WARN: Terminal doesn't have support for colours")
 }
-// do custom colors check
 
 // Init terminal colours
 start_color()
@@ -88,43 +112,45 @@ init_pair(4, Int16(COLOR_WHITE), Int16(use_default_colors()))
 init_pair(5, Int16(COLOR_WHITE), Int16(COLOR_CYAN))
 
 
-var widgets = [Widget]()
+//------------------------------------------------------------------------------
+// MARK: WIDGET SETUP
+//------------------------------------------------------------------------------
 
-// newwin null check
-var gap : Int32 = 1
+
 func computeWidgetLength() -> Int32 {
     return Int32(floor(Double((COLS - (gap * Int32(widgets.count - 1)))) / Double(widgets.count)))
 }
 
 
-
-let  smc = SMC()
+let smc = SMC()
 if (smc.open() == kIOReturnSuccess) {
     HAS_SMC = true
-}
-
-// TODO check if laptop
-let battery = Battery()
-HAS_BATTERY = battery.isLaptop()
-battery.open()
-
-
-if (HAS_SMC) {
     widgets.append(TMPWidget(win: Window()))
     widgets.append(FanWidget(win: Window()))
 }
 
-if (HAS_BATTERY) {
+let battery = Battery()
+if (battery.isLaptop()) {
+    HAS_BATTERY = true
+    battery.open()
     widgets.append(BatteryWidget(win: Window()))
 }
 
+
 var widgetLength = computeWidgetLength()
-
-
 for var i = 0; i < widgets.count; ++i {
     widgets[i].resize(Window(size: (length: widgetLength, width: 1), pos: (x: (widgetLength + gap) * Int32(i), y: 0)))
 }
 
+
+//------------------------------------------------------------------------------
+// MARK: GCD TIMER SETUP
+//------------------------------------------------------------------------------
+
+
+dispatch_source_set_timer(source,
+                          dispatch_time(DISPATCH_TIME_NOW, 0),
+                          FREQ * NSEC_PER_SEC, 0)
 
 dispatch_source_set_event_handler(source, {
     for widget in widgets {
@@ -133,20 +159,25 @@ dispatch_source_set_event_handler(source, {
     refresh()
 })
 
+// We have to resume the the dispatch source as it is paused by default
 dispatch_resume(source)
+
+
+//------------------------------------------------------------------------------
+// MARK: MAIN (EVENT) LOOP - WHERE THE MAGIC HAPPENS :)
+//------------------------------------------------------------------------------
 
 
 var key : Int32 = 0
 var quit = false
 
 while (!quit) {
-    // Why does esc (27) cause such a slow response, as oppossed to something
-    // like 'q'?
+    // TODO: Why does esc (27) cause such a slow response, as oppossed to
+    //       something like 'q'?
     key = getch()
     
-    // has_key check for KEY_RESIZE?
-    
     switch key {
+        // TODO: has_key() check for KEY_RESIZE?
         case KEY_RESIZE:
             // This could be done through GCD signal handler as well
             dispatch_suspend(source)
@@ -163,6 +194,7 @@ while (!quit) {
             quit = true
             break
         case KEY_DOWN:
+            // FIXME: This is just temp, for testing
             var x = getcurx(stdscr)
             var y = getcury(stdscr)
             move(y + 1, x)
@@ -172,5 +204,6 @@ while (!quit) {
     }
 }
 
+// Cleanup
 smc.close()
 battery.close()
