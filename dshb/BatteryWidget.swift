7613748d49dc1eed77fc60a38df90fc34602bb0e
beltex <beltex@users.noreply.github.com>
2015-01-09 11:35:48 -0500

    Cleaner battery widget init

--- dshb/BatteryWidget.swift
+++ dshb/BatteryWidget.swift
@@ -9,27 +9,35 @@ public struct BatteryWidget: Widget {
     var title : WidgetTitle
     var win   : Window
     
+    private let stats: [(name: String, maxValue: Double, unit: Meter.Unit)] =
+               [("Charge", 100.0, Meter.Unit.Percentage),
+                ("Capacity", Double(battery.designCapacity()), Meter.Unit.None),
+                ("Cycles", Double(battery.designCycleCount()), Meter.Unit.None)]
+    
     init(win: Window) {
-        // win.width not in use right now
-        
         self.win = win
-        
-        let titleCoords = Window(size: (length: win.size.length, width: 1), pos: (x:win.pos.x, y:win.pos.y))
-        title = WidgetTitle(title: "Battery", winCoords: titleCoords, colour: COLOR_PAIR(5))        
+        title = WidgetTitle(title: "Battery",
+                            winCoords: Window(size: (length: win.size.length, width: 1),
+                            pos: (x:win.pos.x, y:win.pos.y)),
+                            colour: COLOR_PAIR(5))
 
+        var yShift = 1
+        for stat in stats {
+            meters.append(Meter(name: stat.name,
+                                winCoords: Window(size: (length: win.size.length, width: 1),
+                                                  pos: (x: win.pos.x, y: win.pos.y + yShift)),
+                                max: stat.maxValue,
+                                unit: stat.unit))
+            
+            ++yShift
+        }
         
-        meters.append(Meter(name: "Charge", winCoords : Window(size: (length: win.size.length, width: 1), pos: (x:win.pos.x, y:win.pos.y + 1)), max: 100, unit: Meter.Unit.Percentage))
+        
         meters[0].lowPercentage = 0.2
         meters[0].midPercentage = 0.0
         meters[0].highPercentage = 0.8
-        
         meters[0].lowColour = Int32(3)
         meters[0].highColour = Int32(1)
-
-        
-        meters.append(Meter(name: "Capacity", winCoords : Window(size: (length: win.size.length, width: 1), pos: (x:win.pos.x, y:win.pos.y + 2)), max: Double(battery.designCapacity()), unit: Meter.Unit.None))
-        meters.append(Meter(name: "Cycles", winCoords : Window(size: (length: win.size.length, width: 1), pos: (x:win.pos.x, y:win.pos.y + 3)), max: Double(battery.designCycleCount()), unit: Meter.Unit.None))
-        meters.append(Meter(name: "Health", winCoords : Window(size: (length: win.size.length, width: 1), pos: (x:win.pos.x, y:win.pos.y + 4)), max: 100, unit: Meter.Unit.Percentage))
     }
     
     
@@ -39,11 +47,9 @@ public struct BatteryWidget: Widget {
         
         var v1 = battery.currentCapacity()
         var v2 = battery.cycleCount()
-        var v3 = battery.health()
         
         meters[1].draw(String(v1), percentage:  Double(v1) / Double(battery.designCapacity()))
         meters[2].draw(String(v2), percentage: Double(v2) / Double(battery.designCycleCount()))
-        meters[3].draw(String(Int(v3)), percentage: v3 / 100.0)
     }
     
     
@@ -53,8 +59,6 @@ public struct BatteryWidget: Widget {
         title.resize(win)
         
         var y_pos = win.pos.y + 1 // Becuase of title
-        
-        //meterInverse.resize(Window(size: (length: widgetLength, width: 1), pos: (x: win.pos.x, y: y_pos)))
         for var i = 0; i < meters.count; ++i {
             meters[i].resize(Window(size: (length: widgetLength, width: 1), pos: (x: win.pos.x, y: y_pos)))
             y_pos++
