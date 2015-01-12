efa36048bafe7c56a6adf88ed355ddb33af6f9ed
beltex <beltex@users.noreply.github.com>
2015-01-12 14:49:57 -0500

    Add license headers

--- dshb/main.swift
+++ dshb/main.swift
@@ -4,7 +4,7 @@
 //
 // The MIT License
 //
-// Copyright (C) 2014  beltex <https://github.com/beltex>
+// Copyright (C) 2014, 2015  beltex <https://github.com/beltex>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
